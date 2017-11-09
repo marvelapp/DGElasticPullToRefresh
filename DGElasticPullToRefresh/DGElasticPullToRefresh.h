7061f856ea1efd62edfeaec0e90ee3880c70c425
Ed Wellbrook <edwellbrook@gmail.com>
2017-11-09 18:07:08 +0000

    Update project for carthage

---
+++ DGElasticPullToRefresh/DGElasticPullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  DGElasticPullToRefresh.h
+//  DGElasticPullToRefresh
+//
+//  Created by Ed Wellbrook on 09/11/2017.
+//  Copyright © 2017 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGElasticPullToRefresh.
+FOUNDATION_EXPORT double DGElasticPullToRefreshVersionNumber;
+
+//! Project version string for DGElasticPullToRefresh.
+FOUNDATION_EXPORT const unsigned char DGElasticPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGElasticPullToRefresh/PublicHeader.h>
+
+
